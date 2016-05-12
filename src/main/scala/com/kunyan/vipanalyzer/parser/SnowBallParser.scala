3c1c1dc3830712fae048d4e49edb4bdfb5a24d71
ysrotciv <ysrotciv@gmail.com>
2016-05-12 10:10:27 +0800

    add snow ball vip parser

--- src/main/scala/com/kunyan/vipanalyzer/parser/SnowBallParser.scala
+++ src/main/scala/com/kunyan/vipanalyzer/parser/SnowBallParser.scala
@@ -1,8 +1,18 @@
 package com.kunyan.vipanalyzer.parser
 
+import scala.util.parsing.json.JSON
+
 /**
   * Created by yang on 5/10/16.
   */
 object SnowBallParser {
+
+  def parseJson(jsonString: String): Map[String, String] = {
+
+    val json = JSON.parseFull(jsonString)
+    val map = json.get.asInstanceOf[Map[String, String]]
+
+    map
+  }
 
 }
