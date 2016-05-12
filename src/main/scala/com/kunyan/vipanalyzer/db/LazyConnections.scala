a4d3b7da4dc618d372383494ca9ab8cfc005cd09
ysrotciv <ysrotciv@gmail.com>
2016-05-12 15:31:50 +0800

    添加 mysql 的 lazy 连接

--- src/main/scala/com/kunyan/vipanalyzer/db/LazyConnections.scala
+++ src/main/scala/com/kunyan/vipanalyzer/db/LazyConnections.scala
@@ -1,18 +1,52 @@
 package com.kunyan.vipanalyzer.db
 
-import java.sql.{Connection, DriverManager}
+import java.sql.{DriverManager, PreparedStatement}
+import java.util.Properties
 
 import com.kunyan.vipanalyzer.logger.VALogger
-import org.apache.hadoop.hbase.HBaseConfiguration
+import kafka.producer.{KeyedMessage, Producer, ProducerConfig}
+import org.apache.hadoop.hbase.{HBaseConfiguration, TableName}
 
 import scala.xml.Elem
 
 /**
   * Created by yangshuai on 2016/5/11.
   */
-class LazyConnections(createHbaseConnection: () => org.apache.hadoop.hbase.client.Connection) extends Serializable {
+class LazyConnections(createHbaseConnection: () => org.apache.hadoop.hbase.client.Connection,
+                      createProducer: () => Producer[String, String],
+                      createMySQLConnection: () => PreparedStatement) extends Serializable {
 
-  lazy val hbaseConnection = createHbaseConnection()
+  lazy val hbaseConn = createHbaseConnection()
+
+  lazy val mysqlConn = createMySQLConnection()
+
+  lazy val producer = createProducer()
+
+  def sendTask(topic: String, value: String): Unit = {
+
+    val message = new KeyedMessage[String, String](topic, value)
+
+    try {
+      producer.send(message)
+    } catch {
+      case e: Exception =>
+        e.printStackTrace()
+    }
+  }
+
+  def sendTask(topic: String, values: Seq[String]): Unit = {
+
+    val messages = values.map(x => new KeyedMessage[String, String](topic, x))
+
+    try {
+      producer.send(messages: _*)
+    } catch {
+      case e: Exception =>
+        e.printStackTrace()
+    }
+  }
+
+  def getTable(tableName: String) = hbaseConn.getTable(TableName.valueOf(tableName))
 
 }
 
@@ -29,6 +63,7 @@ object LazyConnections {
       VALogger.warn("create connection")
 
       val connection = org.apache.hadoop.hbase.client.ConnectionFactory.createConnection(hbaseConf)
+
       sys.addShutdownHook {
         connection.close()
       }
@@ -38,7 +73,38 @@ object LazyConnections {
       connection
     }
 
-    new LazyConnections(createHbaseConnection)
+    val createProducer = () => {
+
+      val props = new Properties()
+      props.put("metadata.broker.list", (configFile \ "kafka" \ "brokerList").text)
+      props.put("serializer.class", "kafka.serializer.StringEncoder")
+      props.put("producer.type", "async")
+
+      val config = new ProducerConfig(props)
+      val producer = new Producer[String, String](config)
+
+      sys.addShutdownHook{
+        producer.close()
+      }
+
+      producer
+    }
+
+    val createMySQLConnection = () => {
+
+      Class.forName("com.mysql.jdbc.Driver")
+      val connection = DriverManager.getConnection((configFile \ "mysql" \ "url").text, (configFile \ "mysql" \ "username").text, (configFile \ "mysql" \ "password").text)
+
+      sys.addShutdownHook {
+        connection.close()
+      }
+
+      VALogger.warn("MySQL connection created.")
+
+      connection.prepareStatement("INSERT INTO news_summary (user_id, followers_count) VALUES (?,?)")
+    }
+
+    new LazyConnections(createHbaseConnection, createProducer, createMySQLConnection)
 
   }
 }
